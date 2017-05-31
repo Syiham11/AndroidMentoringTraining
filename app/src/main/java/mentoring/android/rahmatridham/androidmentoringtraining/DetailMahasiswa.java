8bdd11848b7f347170494d21359d79a50b2d08df
rahmatridhamus <rahmatridham@hotmail.com>
2017-05-31 22:02:21 +0700

    Initial commit

---
+++ app/src/main/java/mentoring/android/rahmatridham/androidmentoringtraining/DetailMahasiswa.java
@@ -0,0 +1,13 @@
+package mentoring.android.rahmatridham.androidmentoringtraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailMahasiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_mahasiswa);
+    }
+}
---
+++ app/src/main/java/mentoring/android/rahmatridham/androidmentoringtraining/TambahMahasiswa.java
@@ -0,0 +1,13 @@
+package mentoring.android.rahmatridham.androidmentoringtraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TambahMahasiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_mahasiswa);
+    }
+}
