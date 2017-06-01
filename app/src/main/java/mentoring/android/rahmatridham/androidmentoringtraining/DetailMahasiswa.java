c1e93c909f54276e0e2d3b2f4be86781a72aa084
rahmatridhamus <rahmatridham@hotmail.com>
2017-06-01 23:37:58 +0700

    beres malam keempat

--- app/src/main/java/mentoring/android/rahmatridham/androidmentoringtraining/DetailMahasiswa.java
+++ app/src/main/java/mentoring/android/rahmatridham/androidmentoringtraining/DetailMahasiswa.java
@@ -1,7 +1,13 @@
 package mentoring.android.rahmatridham.androidmentoringtraining;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,5 +27,36 @@ public class DetailMahasiswa extends AppCompatActivity {
         nama = (TextView) findViewById(R.id.textViewNama);
         nim = (TextView) findViewById(R.id.textViewNIM);
 
+        String nama = getIntent().getExtras().getString("nama", "kosong");
+        String nim = getIntent().getExtras().getString("nim", "kosong");
+        String jenisKelamin = getIntent().getExtras().getString("jenisKelamin", "kosong");
+        String noTelpon = getIntent().getExtras().getString("noTelpon", "kosong");
+
+        this.nama.setText(nama);
+        this.nim.setText(nim);
+
+        if (jenisKelamin.equals("L")) {
+            this.gambarOrang.setImageResource(R.drawable.man);
+        } else {
+            this.gambarOrang.setImageResource(R.drawable.woman);
+        }
+
+        nelpon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + getIntent().getExtras().getString("noTelpon", "081280124")));
+                if (ActivityCompat.checkSelfPermission(DetailMahasiswa.this.getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/mentoring/android/rahmatridham/androidmentoringtraining/Home.java
+++ app/src/main/java/mentoring/android/rahmatridham/androidmentoringtraining/Home.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -27,6 +28,20 @@ public class Home extends AppCompatActivity {
         adapter = new ListMahasiswaAdapter(mahasiswaArrayList, this);
         listViewMahasiswa.setAdapter(adapter);
         addMahasiswa();
+
+        listViewMahasiswa.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Mahasiswa mahasiswa = mahasiswaArrayList.get(i);
+                Toast.makeText(Home.this, mahasiswa.getNama(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Home.this,DetailMahasiswa.class);
+                intent.putExtra("nama",mahasiswa.getNama());
+                intent.putExtra("nim",mahasiswa.getNim());
+                intent.putExtra("jenisKelamin",mahasiswa.getJenisKelamin());
+                intent.putExtra("noTelpon",mahasiswa.getNoTelpon());
+                startActivity(intent);
+            }
+        });
 
         fabTambah.setOnClickListener(new View.OnClickListener() {
             @Override
